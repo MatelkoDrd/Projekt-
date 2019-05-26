61153638c7528f674d4290d173c0459634fad690
Mateusz Derda <mateuszderda95@gmail.com>
2019-05-26 21:42:21 +0200

    delete unusable lines

--- core/my_context_processor.py
+++ core/my_context_processor.py
@@ -2,6 +2,5 @@ def my_cp(request):
     ctx = {
         'zalogowany': request.user.is_authenticated,
         'Nazwa użytkownika': request.user.email if request.user.is_authenticated else "Niezalogowany",
-        'niezarejestrowany': request.user.is_anonymous
-    }
+        }
     return ctx
